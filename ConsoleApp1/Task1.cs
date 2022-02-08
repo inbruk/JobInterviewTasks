85449373cba92310c2957b589d8d3436515d00bd
inbruk <35333872+inbruk@users.noreply.github.com>
2022-02-08 10:28:35 +0300

    Add project files.

---
+++ ConsoleApp1/Task1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class A
+    {
+        public virtual void Foo()
+        {
+            Console.Write("Class A");
+        }
+    }
+    public class B : A
+    {
+        public override void Foo()
+        {
+            Console.Write("Class B");
+        }
+    }
+    public class Task1
+    {
+        static public void CheckIt()
+        {
+            //            B obj1 = new A();
+            //            obj1.Foo();
+
+            B obj2 = new B();
+            obj2.Foo();
+
+            A obj3 = new B();
+            obj3.Foo();
+
+            Console.ReadLine();
+        }
+    }
+}
